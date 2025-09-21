46236dd5c945632727921170b61492206efced0d
Alejandro Calixto <97924741+SoyAlejandroCalixto@users.noreply.github.com>
2025-09-21 05:10:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ sudo reflector --latest 10 --age 1 --protocol https --sort rate --save /etc/pacm
 sudo pacman -Rns --noconfirm dolphin vim kitty
 
 # Clone my desktop config
-git clone https://github.com/SoyAlejandroCalixto/arch4devs ~/arch4devs
-cd ~/arch4devs
+git clone https://github.com/SoyAlejandroCalixto/arch4devs $HOME/arch4devs
+cd $HOME/arch4devs
 ./install.sh
-sudo rm -rf ~/arch4devs && sudo rm -rf ~/.git && sudo rm -rf ~/README.md && sudo rm -rf ~/LICENSE && sudo rm -rf ~/.gitignore # Clean repo trash
+sudo rm -rf $HOME/arch4devs && sudo rm -rf $HOME/.git && sudo rm -rf $HOME/README.md && sudo rm -rf $HOME/LICENSE && sudo rm -rf $HOME/.gitignore # Clean repo trash
 
 # Install extra packages
 sudo pacman -S --needed --noconfirm fzf bitwarden
@@ -49,23 +49,23 @@ else
 fi
 
 # Clone neovim config
-git clone https://github.com/SoyAlejandroCalixto/nvim-config ~/.config/nvim
+git clone https://github.com/SoyAlejandroCalixto/nvim-config $HOME/.config/nvim
 
 # Add fnm to path
 echo -e "\neval \"\$(fnm env --use-on-cd --shell zsh)\"" >> $HOME/.zshrc
 
 # Monitors settings
-cat << EOF > ~/.config/hypr/monitors.conf
+cat << EOF > $HOME/.config/hypr/monitors.conf
 monitor=HDMI-A-1,1920x1080@75,0x0,1
 monitor=DP-2,1920x1080@60,1920x0,1
 EOF
 
 # Ranger config and plugins
-git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-echo "default_linemode devicons" >> ~/.config/ranger/rc.conf
-git clone https://github.com/maximtrp/ranger-archives.git ~/.config/ranger/plugins/ranger-archives
-echo "set preview_images true" >> ~/.config/ranger/rc.conf
-echo "set preview_images_method iterm2" >> ~/.config/ranger/rc.conf
+git clone https://github.com/alexanderjeurissen/ranger_devicons $HOME/.config/ranger/plugins/ranger_devicons
+echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+git clone https://github.com/maximtrp/ranger-archives.git $HOME/.config/ranger/plugins/ranger-archives
+echo "set preview_images true" >> $HOME/.config/ranger/rc.conf
+echo "set preview_images_method iterm2" >> $HOME/.config/ranger/rc.conf
 
 # Cloudflare Warp
 sudo systemctl enable warp-svc
